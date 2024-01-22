59877280a401d10eba90ca5db4ac3047f2310328
Haidaradlan12 <adlannufahaidar12@gmail.com>
2024-01-22 15:52:01 +0700

    Initial commit

---
+++ Assets/Asset Script/UIManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIManager : MonoBehaviour
+{
+    public RectTransform Menu1, Menu2, Button, Informasi, Petunjuk;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Menu1.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        Menu2.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        Button.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        Informasi.DOAnchorPos(new Vector2(-2000, 0), 1.0f);
+        Petunjuk.DOAnchorPos(new Vector2(-2000, 0), 1.0f);
+    }
+    public void Kembalibutton()
+    {
+        Menu1.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        Menu2.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        Button.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        Informasi.DOAnchorPos(new Vector2(-2000, 0), 1.0f);
+        Petunjuk.DOAnchorPos(new Vector2(-2000, 0), 1.0f);
+    }
+    public void Informasibutton()
+    {
+        Menu1.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        Menu2.DOAnchorPos(new Vector2(-2000, 0), 1.0f);
+        Button.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        Informasi.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        Petunjuk.DOAnchorPos(new Vector2(-2000, 0), 1.0f);
+    }
+    public void Petunjukbutton()
+    {
+        Menu1.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        Menu2.DOAnchorPos(new Vector2(-2000, 0), 1.0f);
+        Button.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        Informasi.DOAnchorPos(new Vector2(-2000, 0), 1.0f);
+        Petunjuk.DOAnchorPos(new Vector2(0, 0), 1.0f);
+    }
+}
---
+++ Assets/Asset Script/UIManagerMain.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIManagerMain : MonoBehaviour
+{
+    public RectTransform Popup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Popup.DOAnchorPos(new Vector2(0, -3000), 1.0f);
+    }
+    public void Hidebutton()
+    {
+        Popup.DOAnchorPos(new Vector2(0, -3000), 1.0f);
+    }
+    public void Unhidebutton()
+    {
+        Popup.DOAnchorPos(new Vector2(0, 0), 1.0f);
+    }
+}
